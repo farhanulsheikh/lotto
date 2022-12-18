8f63686d091fac9dbc6e3a7e87b1b98742f0fb13
Farhan Sheikh <farhanul.sheikh@outlook.com>
2022-12-18 01:40:31 -0500

    add state to Lottery Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Lottery from './Lottery';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+
+
+        <Lottery title="LOTTO 6/49" />
+        <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
+      </div>
+    )
+  }
+
 }
+
 
 export default App;
